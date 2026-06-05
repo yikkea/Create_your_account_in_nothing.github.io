ab581a704b0c017f39b9df186a0cabf9bfc38902
yikkea <farhasifkhan25@gmail.com>
2026-06-05 14:59:38 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,5 @@
+function padji() {
+    let name = document.querySelector("input").value
+    let p = document.getElementById("cong")
+    p.innerHTML= "Congrats, " + name + " your account has succefully created!"
+}
